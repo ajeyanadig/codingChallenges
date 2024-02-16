eff237b04c8d044503d013d95c0b039f48a41586
ajeyanadig <ajeyanadig@gmail.com>
2024-02-16 23:06:52 +0530

    Revision of last topics done

---
+++ playground/script.js
@@ -0,0 +1,113 @@
+"use strict";
+//Some fun questions I wanna take a crack at
+/*
+Coding Challenge #2 -> THE BONUS ONE
+Let's continue with our football betting app! Keep using the 'game' variable from before.
+Your tasks:
+1. Loopoverthegame.scoredarrayandprinteachplayernametotheconsole, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Usealooptocalculatetheaverageoddandlogittotheconsole(Wealready studied how to calculate averages, you can go check if you don't remember)
+3. Printthe3oddstotheconsole,butinaniceformattedway,exactlylikethis:
+Odd of victory Bayern Munich: 1.33 Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). Hint: Note how the odds and the game objects have the same property names 😉
+4. Bonus:Createanobjectcalled'scorers'whichcontainsthenamesofthe players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+     {
+       Gnarby: 1,
+       Hummels: 1,
+       Lewandowski: 2
+}*/
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+for (let [key, val] of game.scored.entries()) {
+  console.log(`Goal ${key}: ${val}`);
+}
+let total = 0;
+for (let val of Object.values(game.odds)) {
+  total += val;
+}
+console.log(total / Object.values(game.odds).length);
+// exmaple Odd of victory Bayern Munich: 1.33 Odd of draw: 3.25
+let oddsArr = Object.entries(game.odds);
+for (let [key, val] of oddsArr) {
+  console.log(`Odd of ${game[key] ? `victory ${game[key]}` : `draw`}: ${val}`);
+}
+//tricky for using short syntax but basically using falsy values, and ternary operators to check existence of a key and assigning it with correct count as and when needed. Last time I used a brute force to run over the arrays
+const scorers = {};
+for (let val of game.scored) {
+  scorers[val] ? scorers[val]++ : (scorers[val] = 1);
+}
+console.log(scorers);
+///STRINGS THERE'S A FUN QUESTION
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+const inputElele = document.querySelector("textarea");
+const buttonEle = document.querySelector("button");
+buttonEle.appendChild(document.createTextNode("Submit "));
+//27th MORNING QUESTION TO DO
